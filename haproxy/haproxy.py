0aed37ac84502279243e1637c1a977f6b1101107
Gil Forcada <gil.forcada@freitag.de>
2013-12-11 18:19:12 +0100

    Add HaproxyLogLine class with tests
    HaproxyLogLine class represents a single Haproxy log line, its
    attributes are the fields found in a typical Haproxy log line.

--- haproxy/haproxy.py
+++ haproxy/haproxy.py
@@ -1,4 +1,6 @@
 # -*- encoding: utf-8 -*-
+from datetime import datetime
+
 import re
 
 
@@ -45,3 +47,116 @@ HTTP_REQUEST_REGEX = re.compile(
     r'(?P<path>(/[/\w:,;\.#$!?=&@%_+\'\*~|\(\)\[\]-]+)+)'
     r'\s+(?P<protocol>\w+/\d\.\d)'
 )
+
+
+class HaproxyLogLine(object):
+
+    def __init__(self, line):
+        """For a description of every field see:
+        http://cbonte.github.io/haproxy-dconv/configuration-1.4.html#8.2.3
+
+        The declarations here follow the syntax of the log file, thus
+        backend_name is found after fronted_name and before server_name.
+
+        This helps keeping an eye on where a specific field is stored.
+        """
+        self.client_ip = None
+        self.client_port = None
+
+        # raw string from log line and its python datetime version
+        self.raw_accept_date = None
+        self.accept_date = None
+
+        self.frontend_name = None
+        self.backend_name = None
+        self.server_name = None
+
+        self.time_wait_request = None
+        self.time_wait_queues = None
+        self.time_connect_server = None
+        self.time_wait_response = None
+        self.total_time = None
+
+        self.status_code = None
+        self.bytes_read = None
+
+        # not used by now
+        self.captured_request_cookie = None
+        self.captured_response_cookie = None
+
+        # not used by now
+        self.termination_state = None
+
+        self.connections_active = None
+        self.connections_frontend = None
+        self.connections_backend = None
+        self.connections_server = None
+        self.retries = None
+
+        self.queue_server = None
+        self.queue_backend = None
+
+        self.captured_request_headers = None
+        self.captured_response_headers = None
+
+        self.raw_http_request = None
+        self.http_request_method = None
+        self.http_request_path = None
+        self.http_request_protocol = None
+
+        self.raw_line = line
+
+        self.parsed = self._parse_line(line)
+
+    def _parse_line(self, line):
+        matches = HAPROXY_LINE_REGEX.match(line)
+        if matches is None:
+            return False
+
+        self.client_ip = matches.group('client_ip')
+        self.client_port = int(matches.group('client_port'))
+
+        self.raw_accept_date = matches.group('accept_date')
+        self.accept_date = self._parse_accept_date()
+
+        self.frontend_name = matches.group('frontend_name')
+        self.backend_name = matches.group('backend_name')
+        self.server_name = matches.group('server_name')
+
+        self.time_wait_request = int(matches.group('tq'))
+        self.time_wait_queues = int(matches.group('tw'))
+        self.time_connect_server = int(matches.group('tc'))
+        self.time_wait_response = int(matches.group('tr'))
+        self.total_time = matches.group('tt')
+
+        self.status_code = matches.group('status_code')
+        self.bytes_read = matches.group('bytes_read')
+
+        self.connections_active = matches.group('act')
+        self.connections_frontend = matches.group('fe')
+        self.connections_backend = matches.group('be')
+        self.connections_server = matches.group('srv')
+        self.retries = matches.group('retries')
+
+        self.queue_server = matches.group('queue_server')
+        self.queue_backend = matches.group('queue_backend')
+
+        self.captured_request_headers = matches.group('request_headers')
+        self.captured_response_headers = matches.group('response_headers')
+        if matches.group('headers') is not None:
+            self.captured_request_headers = matches.group('headers')
+
+        self.raw_http_request = matches.group('http_request')
+        self._parse_http_request()
+
+        return True
+
+    def _parse_accept_date(self):
+        return datetime.strptime(self.raw_accept_date, '%d/%b/%Y:%H:%M:%S')
+
+    def _parse_http_request(self):
+        matches = HTTP_REQUEST_REGEX.match(self.raw_http_request)
+        if matches:
+            self.http_request_method = matches.group('method')
+            self.http_request_path = matches.group('path')
+            self.http_request_protocol = matches.group('protocol')
---
+++ haproxy/tests/test_haproxy_log_line.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from haproxy.haproxy import HaproxyLogLine
+
+import unittest
+
+
+LINE = '{0} {1} {2} [{3}] {4} {5} {6} - - ---- {7} {8} {9} "{10}"'
+
+
+class HaproxyLogLineTest(unittest.TestCase):
+
+    def setUp(self):
+        self.syslog_date = 'Dec  9 13:01:26'
+        self.process_name_and_pid = 'localhost haproxy[28029]:'
+
+        self.client_ip = '127.0.0.1'
+        self.client_port = '2345'
+
+        self.accept_date = '09/Dec/2013:12:59:46.633'
+
+        self.frontend_name = 'loadbalancer'
+        self.backend_name = 'default'
+        self.server_name = 'instance8'
+
+        self.tq = '0'
+        self.tw = '51536'
+        self.tc = '1'
+        self.tr = '48082'
+        self.tt = '99627'
+
+        self.status = '200'
+        self.bytes = '83285'
+
+        self.act = '87'
+        self.fe = '89'
+        self.be = '98'
+        self.srv = '1'
+        self.retries = '20'
+
+        self.queue_server = '2'
+        self.queue_backend = '67'
+        self.headers = '{77.24.148.74}'
+        self.http_request = 'GET /path/to/image HTTP/1.1'
+
+    def _build_test_string(self):
+        client_ip_and_port = '{0}:{1}'.format(self.client_ip,
+                                              self.client_port)
+        server_names = '{0} {1}/{2}'.format(self.frontend_name,
+                                            self.backend_name,
+                                            self.server_name)
+        timers = '{0}/{1}/{2}/{3}/{4}'.format(self.tq,
+                                              self.tw,
+                                              self.tc,
+                                              self.tr,
+                                              self.tt)
+        status_and_bytes = '{0} {1}'.format(self.status, self.bytes)
+        connections_and_retries = '{0}/{1}/{2}/{3}/{4}'.format(self.act,
+                                                               self.fe,
+                                                               self.be,
+                                                               self.srv,
+                                                               self.retries)
+        queues = '{0}/{1}'.format(self.queue_server, self.queue_backend)
+
+        log_line = LINE.format(
+            self.syslog_date,
+            self.process_name_and_pid,
+            client_ip_and_port,
+            self.accept_date,
+            server_names,
+            timers,
+            status_and_bytes,
+            connections_and_retries,
+            queues,
+            self.headers,
+            self.http_request,
+        )
+        return log_line
+
+    def test_haproxy_log_line_default_values(self):
+        raw_line = self._build_test_string()
+        log_line = HaproxyLogLine(raw_line)
+
+        self.assertEqual(raw_line, log_line.raw_line)
+        self.assertEqual(self.client_ip, log_line.client_ip)
+        self.assertEqual(int(self.client_port), log_line.client_port)
+
+        self.assertTrue(log_line.raw_accept_date in self.accept_date)
+
+        self.assertEqual(self.frontend_name, log_line.frontend_name)
+        self.assertEqual(self.backend_name, log_line.backend_name)
+        self.assertEqual(self.server_name, log_line.server_name)
+
+        self.assertEqual(int(self.tq), log_line.time_wait_request)
+        self.assertEqual(int(self.tw), log_line.time_wait_queues)
+        self.assertEqual(int(self.tc), log_line.time_connect_server)
+        self.assertEqual(int(self.tr), log_line.time_wait_response)
+        self.assertEqual(self.tt, log_line.total_time)
+
+        self.assertEqual(self.status, log_line.status_code)
+        self.assertEqual(self.bytes, log_line.bytes_read)
+
+        self.assertEqual(self.act, log_line.connections_active)
+        self.assertEqual(self.fe, log_line.connections_frontend)
+        self.assertEqual(self.be, log_line.connections_backend)
+        self.assertEqual(self.srv, log_line.connections_server)
+        self.assertEqual(self.retries, log_line.retries)
+
+        self.assertEqual(self.queue_server, log_line.queue_server)
+        self.assertEqual(self.queue_backend, log_line.queue_backend)
+
+        self.assertEqual(self.headers, log_line.captured_request_headers)
+        self.assertEqual(None, log_line.captured_response_headers)
+
+        self.assertEqual(self.http_request, log_line.raw_http_request)
+
+        self.assertTrue(log_line.parsed)
+
+    def test_haproxy_log_line_unused_values(self):
+        raw_line = self._build_test_string()
+        log_line = HaproxyLogLine(raw_line)
+
+        self.assertEqual(log_line.captured_request_cookie, None)
+        self.assertEqual(log_line.captured_response_cookie, None)
+        self.assertEqual(log_line.termination_state, None)
+
+    def test_haproxy_log_line_datetime_value(self):
+        raw_line = self._build_test_string()
+        log_line = HaproxyLogLine(raw_line)
+
+        self.assertTrue(isinstance(log_line.accept_date, datetime))
+
+    def test_haproxy_log_line_http_request_values(self):
+        method = 'GET'
+        path = '/path/to/image'
+        protocol = 'HTTP/1.1'
+        self.http_request = '{0} {1} {2}'.format(method, path, protocol)
+        raw_line = self._build_test_string()
+        log_line = HaproxyLogLine(raw_line)
+
+        self.assertEqual(log_line.http_request_method, method)
+        self.assertEqual(log_line.http_request_path, path)
+        self.assertEqual(log_line.http_request_protocol, protocol)
+
+    def test_haproxy_log_line_invalid(self):
+        """Check that if a log line can not be parsed with the regular
+        expression, 'parsed' is False.
+        """
+        self.bytes = 'wrooooong'
+        raw_line = self._build_test_string()
+        log_line = HaproxyLogLine(raw_line)
+
+        self.assertFalse(log_line.parsed)
