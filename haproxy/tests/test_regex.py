a09fd199144951caa14ea469b6f8c84ff9aead37
Gil Forcada <gil.forcada@freitag.de>
2014-02-03 14:21:10 +0100

    Allow curly brackets on path

--- haproxy/tests/test_regex.py
+++ haproxy/tests/test_regex.py
@@ -402,3 +402,10 @@ class HttpRequestRegexTest(unittest.TestCase):
         matches = HTTP_REQUEST_REGEX.match(line)
 
         self.assertEqual(matches.group('path'), self.path)
+
+    def test_http_request_regex_with_curly_brackets_symbol(self):
+        self.path = '/georg}von{grote/\\'
+        line = self._build_test_request()
+        matches = HTTP_REQUEST_REGEX.match(line)
+
+        self.assertEqual(matches.group('path'), self.path)
