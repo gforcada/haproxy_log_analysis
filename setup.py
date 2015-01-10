e8ef00bd6576ba5e43dfb04fe74cada1c3c48e56
Gil Forcada <gforcada@gnome.org>
2015-01-10 08:31:01 +0100

    Test also against python 3.4

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Intended Audience :: System Administrators',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: Log Analysis',
     ],
     keywords='haproxy log analysis report',
