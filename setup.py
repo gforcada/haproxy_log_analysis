8375d5d56eda802aab228e884b62e8c74de3e817
Gil Forcada <gil.gnome@gmail.com>
2019-06-10 00:07:49 +0200

    Back to development: 3.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '3.0.0'
+version = '3.0.1.dev0'
 
 description = (
     'Haproxy log analyzer that tries to gives an insight of ' 'what\'s going on'
