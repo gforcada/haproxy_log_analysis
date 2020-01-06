2098e808c9fb2db72d81488524f66549fb49cc89
Gil Forcada <gil.gnome@gmail.com>
2020-01-06 23:34:57 +0100

    Back to development: 4.1.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '4.1.0'
+version = '4.1.1.dev0'
 
 description = "Haproxy log analyzer that tries to gives an insight of what's going on"
 
